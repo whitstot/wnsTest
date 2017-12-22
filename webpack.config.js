c742e1ada7d6ca8327d51f4a2a19246a02c86b6f
Whitney Nicole Stotler <user@186-237-170-69-in-addr-arpa.brightok.net>
2017-12-22 12:13:57 -0600

    added webpack config of my own

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,85 @@
-/* uncomment the following block and the respective line within the plugins section for the webpack config to get this working
-*/
-// const WebpackShellPlugin = require('webpack-shell-plugin');
-// Current build path represents where the file needs to go to
-// var MOVE_FILE_TO = 'Y:/marsjs/js/build/CustomizeTable/distribute/';
-// var RECOMPILE_ON_THIS_FILE = MOVE_FILE_TO + '/bundle.js';
-// var shellCommand2 = 'mv ./dist/bundle.js ' + MOVE_FILE_TO;
-// var _onBuildEnd = shellCommand2;
+const path              = require('path');
+const webpack           = require('webpack');
+const htmlPlugin        = require('html-webpack-plugin');
+const openBrowserPlugin = require('open-browser-webpack-plugin'); 
+const dashboardPlugin   = require('webpack-dashboard/plugin');
+const autoprefixer      = require('autoprefixer'); 
 
-const webpack = require('webpack'); //this is used for a production build
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  images:path.join(__dirname,'src/assets/'),
+  build: path.join(__dirname, 'dist')
+};
+
+const options = {
+  host:'localhost',
+  port:'1234'
+};
 
 module.exports = {
-  entry: './js/index.js',
+  entry: {
+    './js/index.js'
+  },
   output: {
     path: __dirname,
     filename: './dist/bundle.js'
   },
+  devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      stats: 'errors-only',
+      host: options.host,
+      port: options.port 
+    },
   module: {
     loaders: [
-        {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-              plugins: ['transform-runtime'],
-              presets: ['es2015', 'stage-0', 'react'],
-            }
-        },
-        {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file?name=roboto/[name].[ext]'
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015']
         }
-        // {
-        //     test: /\.css$/,
-        //     loader: 'css-loader',
-        //     // query: {
-        //     //   modules: true,
-        //     //   localIdentName: '[name]__[local]__[hash:base64:5]'
-        //     // }
-        // },        
-        // {
-        //     test: /\.css$/,
-        //     loader: 'style-loader'
-        // }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css', 'postcss'],
+        include:PATHS.app
+      },
+      {
+        test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,        
+        loader: 'file',
+        query: {
+          name: '[path][name].[ext]'
+        }
+      },      
     ]
   },
-	plugins: [
-	// 	// new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:['echo "Webpack End"']})
-	// 	new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:[_onBuildEnd]})
-		// new webpack.DefinePlugin({
-		// 	'process.env': {
-		// 		NODE_ENV: JSON.stringify('production')
-		// 	}
-		// }),
-		// new webpack.optimize.UglifyJsPlugin()
+  postcss: function() {
+    return [
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9',
+        ]
+      }),
+    ];
+  },
+  plugins:[
+    new dashboardPlugin(),
+    new webpack.HotModuleReplacementPlugin({
+        multiStep: true
+    }),
+    new htmlPlugin({
+      template:path.join(PATHS.app,'index.html'),
+      inject:'body'
+    }),
+    new openBrowserPlugin({
+      url: `http://${options.host}:${options.port}`
+    })
   ]
-  
-}
+};
